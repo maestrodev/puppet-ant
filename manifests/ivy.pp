96393997fe98abf44a71b6fc8fa66430bb4202a1
Etienne Pelletier <epelletier@maestrodev.com>
2012-09-25 10:22:18 -0500

    Added missing curly braces.

--- manifests/ivy.pp
+++ manifests/ivy.pp
@@ -2,16 +2,16 @@ class ant::ivy($version="2.2.0") {
   include ant::params
 
   wget::fetch { "ivy":
-    source => "http://mirrors.gigenet.com/apache//ant/ivy/$version/apache-ivy-$version-bin.tar.gz",
-    destination => "${ant::params::srcdir}/apache-ivy-$version-bin.tar.gz",
+    source => "http://mirrors.gigenet.com/apache//ant/ivy/${version}/apache-ivy-${version}-bin.tar.gz",
+    destination => "${ant::params::srcdir}/apache-ivy-${version}-bin.tar.gz",
     require => File[ "/usr/bin/ant" ]
   } ->
-  exec { "tar zxvf ${ant::params::srcdir}/apache-ivy-$version-bin.tar.gz":
+  exec { "tar zxvf ${ant::params::srcdir}/apache-ivy-${version}-bin.tar.gz":
     cwd => "/usr/share",
     path => "/bin/:/usr/bin",
-    creates => "/usr/share/apache-ivy-$version"
+    creates => "/usr/share/apache-ivy-${version}"
   } ->
-  file { "/usr/share/apache-ant-${ant::params::version}/lib/ivy-$version.jar":
-    ensure => "/usr/share/apache-ivy-$version/ivy-$version.jar",
+  file { "/usr/share/apache-ant-${ant::params::version}/lib/ivy-${version}.jar":
+    ensure => "/usr/share/apache-ivy-${version}/ivy-${version}.jar",
   }
 }
