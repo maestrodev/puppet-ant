184c805f931664057db1d0285069f3925006f1a7
Carlos Sanchez <csanchez@maestrodev.com>
2012-11-09 15:18:20 +0100

    Add classes to install maven and sonar task libraries

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -6,6 +6,8 @@
 # [source_url] The location of the ant library jar file.
 define ant::lib($version, $source_url) {
 
+  include ant::params
+
   wget::fetch { "${name}-antlib":
     source      => $source_url,
     destination => "/usr/share/apache-ant-${ant::params::version}/lib/${name}-$version.jar",
